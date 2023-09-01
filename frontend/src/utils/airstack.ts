d23b92de89590d8a3ccb49660103a3e03a0e574a
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2023-09-01 23:03:43 +0200

    Add airstack social resolving

---
+++ frontend/src/utils/airstack.ts
@@ -0,0 +1,163 @@
+const { JSDOM } = require('jsdom');
+
+const dom = new JSDOM();
+(global as any).DOMParser = dom.window.DOMParser;
+// ---- debug <-
+
+const AIRSTACK_API_ENDPOINT: string = "https://api.airstack.xyz/gql";
+const AIRSTACK_API_KEY: string = "7468f364d6bd4943afa09c0ca51d2b94"; // Replace with your API key
+
+interface SocialMediaInfo {
+    name: string;
+    avatar: string | null;
+    profileInfo: {
+        dappName: string;
+        profileName: string;
+        userId: string;
+        userAddress: string;
+    };
+}
+
+async function getSocialMediaInfo(ethAddresses: string[]): Promise<SocialMediaInfo[]> {
+    const results = await Promise.all(ethAddresses.map(async ethAddress => {
+        const ensProfileName = await getENSProfileName(ethAddress);
+        console.log(ensProfileName)
+        let avatarUrl = null;
+        if (ensProfileName) {
+            avatarUrl = getAvatarUrl(ensProfileName);
+        }
+
+        let info = await getProfileFromDapp(ethAddress, 'farcaster');
+        if (!info) {
+            info = await getProfileFromDapp(ethAddress, 'lens');
+        }
+        if (!info) {
+            info = await getENSProfileInfo(ethAddress);
+        }
+
+        if (info) {
+            info.avatar = avatarUrl;
+        }
+
+        return info;
+    }));
+
+    // Filter out null values
+    return results.filter(result => result !== null) as SocialMediaInfo[];
+}
+
+async function getProfileFromDapp(ethAddress: string, dappName: string): Promise<SocialMediaInfo | null> {
+    const query = `
+        query {
+            Socials(input: {filter: {identity: {_in: ["${ethAddress}"]}, dappName: {_eq: ${dappName}}}, blockchain: ethereum}) {
+                Social {
+                    dappName
+                    profileName
+                    userId
+                    userAddress
+                }
+            }
+        }
+    `;
+
+    const response = await fetch(AIRSTACK_API_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${AIRSTACK_API_KEY}`
+        },
+        body: JSON.stringify({ query })
+    });
+
+    const responseData = await response.json();
+    const data = responseData.data;
+
+    if (data && data.Socials && data.Socials.Social && data.Socials.Social.length > 0) {
+        const socialInfo = data.Socials.Social[0];
+        return {
+            name: socialInfo.profileName,
+            avatar: null, // Will be filled later
+            profileInfo: socialInfo
+        };
+    }
+    return null;
+}
+
+async function getENSProfileName(ethAddress: string): Promise<string | null> {
+    const query = `
+        query {
+            Domain(input: {resolvedAddress: "${ethAddress}", blockchain: ethereum}) {
+                name
+                owner
+                resolvedAddress
+            }
+        }
+    `;
+
+    const response = await fetch(AIRSTACK_API_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${AIRSTACK_API_KEY}`
+        },
+        body: JSON.stringify({ query })
+    });
+
+    const responseData = await response.json();
+    const data = responseData.data;
+    console.log('DEBUG:', data)
+
+    if (data && data.Domain && data.Domain.name) {
+        return data.Domain.name;
+    }
+    return null;
+}
+
+async function getENSProfileInfo(ethAddress: string): Promise<SocialMediaInfo | null> {
+    const profileName = await getENSProfileName(ethAddress);
+    if (profileName) {
+        return {
+            name: profileName,
+            avatar: null, // Will be filled later
+            profileInfo: {
+                dappName: 'ens',
+                profileName: profileName,
+                userId: ethAddress, // Assuming userId is the ethAddress for ENS
+                userAddress: ethAddress
+            }
+        };
+    }
+    return null;
+}
+
+function getAvatarUrl(profileName: string): string | null {
+    return `https://metadata.ens.domains/mainnet/avatar/${profileName}`;
+}
+
+// Example of request: getSocialMediaInfo(["0xeB1c22baACAFac7836f20f684C946228401FF01C", "0x6f73ea756bd57d3adcafb73a4f5fcd750ec1c387"]).then(info => { console.log(info); });
+
+// Example requests results:
+// ```
+// [
+//     {
+//       name: 'richerd.lens',
+//       avatar: 'https://metadata.ens.domains/mainnet/avatar/jnsilva.eth',
+//       profileInfo: {
+//         dappName: 'lens',
+//         profileName: 'richerd.lens',
+//         userId: '0xeb1c22baacafac7836f20f684c946228401ff01c',
+//         userAddress: '0xeb1c22baacafac7836f20f684c946228401ff01c'
+//       }
+//     },
+//     {
+//       name: 'nobuilder.eth',
+//       avatar: 'https://metadata.ens.domains/mainnet/avatar/libred.eth',
+//       profileInfo: {
+//         dappName: 'farcaster',
+//         profileName: 'nobuilder.eth',
+//         userId: '3017',
+//         userAddress: '0x0d43fb59fe8e5a8249da942d64af5cbc7116c5a5'
+//       }
+//     }
+//   ]
+// ```
