2b3e055a49b2ebb0f347ab8e47e687014ede4688
radekskrabucha <rskrabucha13@gmail.com>
2023-09-01 20:15:26 +0200

    feat(seo): updated seo metatags

--- frontend/src/config/seo.ts
+++ frontend/src/config/seo.ts
@@ -1,35 +1,15 @@
 import { type Metadata } from 'next'
 import { AppName } from './app'
 
-export const FaviconURL =
-  'https://cdn.gameswift.io/gameswift/shared/images/favicon.png'
-export const OGImageUrl =
-  'https://cdn.gameswift.io/gameswift/shared/images/og-gameswift.png'
-export const GameSwiftTwitterHandle = 'GameSwift_io'
-
 export const Description =
-  'A starter for Next.js using app router with TypeScript and Tailwind CSS.'
+  'Challenge accepted! A platform for people who want to challenge themselves and others to do better.'
 
 export const DefaultSEOTags: Metadata = {
   description: Description,
   openGraph: {
     type: 'website',
     locale: 'en_IE',
-    siteName: AppName,
-    images: [
-      {
-        url: OGImageUrl,
-        width: 1200,
-        height: 630,
-        alt: AppName,
-        type: 'image/png'
-      }
-    ]
+    siteName: AppName
   },
-  twitter: {
-    card: 'summary_large_image',
-    site: GameSwiftTwitterHandle
-  },
-  themeColor: '#1F2128',
-  icons: FaviconURL
+  themeColor: '#1F2128'
 }
