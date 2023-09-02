2e15086b64598907d39ef1889c826e36c8f2ab54
Maciej Wojda <mwx27@protonmail.com>
2023-09-02 12:55:30 +0200

    contract details

---
+++ frontend/src/config/contract.ts
@@ -0,0 +1,8 @@
+import * as abi from './ChallengeFactoryABI.json'
+
+const address: `0x${string}` = '0x7d44b7CCF991Bd5f8383F4e13Bec5333B32f9d41'
+
+export const contractDetails = {
+  abi,
+  address
+}
